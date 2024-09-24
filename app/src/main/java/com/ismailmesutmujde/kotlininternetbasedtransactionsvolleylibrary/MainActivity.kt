aebf027157bbe1c157b6d227d4905933b77cff38
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-24 13:57:20 +0300

    updated: volley library - delete (data deleting) process

--- app/src/main/java/com/ismailmesutmujde/kotlininternetbasedtransactionsvolleylibrary/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlininternetbasedtransactionsvolleylibrary/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.ismailmesutmujde.kotlininternetbasedtransactionsvolleylibrary
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +13,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // this result was taken from here: https://kasimadalan.pe.hu/kisiler/delete_kisiler.php
+
+        deletePerson()
+    }
+
+    fun deletePerson() {
+        val url = "https://kasimadalan.pe.hu/kisiler/delete_kisiler.php"
+
+        val request = object : StringRequest(Method.POST, url, Response.Listener { answer->
+            Log.e("Delete Process Answer", answer)
+        }, Response.ErrorListener { e -> e.printStackTrace() }) {
+            override fun getParams(): MutableMap<String, String>? {
+                val params = HashMap<String, String>()
+                params["kisi_id"] = "380"
+                return params
+            }
+        }
+        Volley.newRequestQueue(this@MainActivity).add(request)
     }
 }

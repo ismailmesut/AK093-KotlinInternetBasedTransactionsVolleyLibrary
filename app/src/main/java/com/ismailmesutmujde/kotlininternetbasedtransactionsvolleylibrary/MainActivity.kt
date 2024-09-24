d689a596850e4938d9b5fd66257c08b08be4130d
ismailmesut <ismailmesutmujde@gmail.com>
2024-09-24 14:21:36 +0300

    updated: volley library - update (data updating) process

--- app/src/main/java/com/ismailmesutmujde/kotlininternetbasedtransactionsvolleylibrary/MainActivity.kt
+++ app/src/main/java/com/ismailmesutmujde/kotlininternetbasedtransactionsvolleylibrary/MainActivity.kt
@@ -13,10 +13,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // this result was taken from here: https://kasimadalan.pe.hu/kisiler/delete_kisiler.php
+        // this result was taken from here: http://kasimadalan.pe.hu/kisiler/tum_kisiler.php
 
         //deletePerson()
-        insertPerson()
+        //insertPerson()
+        updatePerson()
     }
 
     fun deletePerson() {
@@ -36,7 +37,6 @@ class MainActivity : AppCompatActivity() {
 
     fun insertPerson() {
         val url = "http://kasimadalan.pe.hu/kisiler/insert_kisiler.php"
-
         val request = object : StringRequest(Method.POST, url, Response.Listener { answer->
             Log.e("Insert Process Answer", answer)
         },Response.ErrorListener { e -> e.printStackTrace() }) {
@@ -47,7 +47,22 @@ class MainActivity : AppCompatActivity() {
                 return params
             }
         }
+        Volley.newRequestQueue(this@MainActivity).add(request)
+    }
 
+    fun updatePerson() {
+        val url = "http://kasimadalan.pe.hu/kisiler/update_kisiler.php"
+        val request = object : StringRequest(Method.POST, url, Response.Listener { answer->
+            Log.e("Update Process Answer", answer)
+        },Response.ErrorListener { e -> e.printStackTrace() }) {
+            override fun getParams(): MutableMap<String, String>? {
+                val params = HashMap<String, String>()
+                params["kisi_id"] = "17721"
+                params["kisi_ad"] = "ismail2"
+                params["kisi_tel"] = "123123000"
+                return params
+            }
+        }
         Volley.newRequestQueue(this@MainActivity).add(request)
     }
 }
